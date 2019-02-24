099cb8927278af9d79336a0b33703d1c8c1d7f3b
PeterSandwich <1120491948@qq.com>
2019-02-24 11:23:22 +0800

    fix

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  websocketUrl: "ws://http://139.199.196.31:9988/ws",
-  apiUrl: "http://http://139.199.196.31:9988/api"
+  websocketUrl: "ws://139.199.196.31:9988/ws",
+  apiUrl: "http://139.199.196.31:9988/api"
 };
